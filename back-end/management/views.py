357a1e553b55e79a971c8b67f44031dce1f0dde7
lyf15 <liuyf278@mail2.sysu.edu.cn>
2024-12-09 17:48:44 +0800

    Update

--- back-end/management/views.py
+++ back-end/management/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+# from .models import 
 
 # Create your views here.
+
+
+def detail(request, question_id):
+    return HttpResponse()
+
+
+
+
+def login(request):
+    # latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    template = loader.get_template("login.html")
+    return HttpResponse(template.render(None, request))
