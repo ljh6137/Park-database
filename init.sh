5aacbb29ccd01e8d26d184245bf49adc733c5fe5
lyf15 <liuyf278@mail2.sysu.edu.cn>
2024-12-25 02:24:35 +0800

    add scripts

--- init.sh
+++ init.sh
@@ -4,7 +4,7 @@
 DB_NAME="demo"
 DB_USER="your_username"  # 修改为创建数据库的用户
 DB_PASSWORD="your_password"  # 修改为数据库的密码
-DATA_FILE="/path/to/your/data.sql"  # 数据文件路径，假设为 SQL 文件
+DATA_FILE="management.sql"  # 数据文件路径，假设为 SQL 文件
 
 # 创建数据库并赋予权限
 echo "创建数据库 $DB_NAME..."
@@ -12,6 +12,9 @@ psql -U $DB_USER -c "CREATE DATABASE $DB_NAME;"
 
 echo "赋予 $DB_USER 对数据库 $DB_NAME 的所有权限..."
 psql -U $DB_USER -c "GRANT ALL PRIVILEGES ON DATABASE $DB_NAME TO $DB_USER;"
+
+python back-end\manage.py makemigrations
+python back-end\manage.py migrate
 
 # 导入数据
 echo "开始导入数据..."
